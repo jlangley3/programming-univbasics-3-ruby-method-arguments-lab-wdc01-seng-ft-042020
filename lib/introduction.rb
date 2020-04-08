bc0b268a74618f861f6fdbee4cfe7ac5a3e35f43
Jesse Langley <jlangleyiii@aol.com>
2020-04-08 05:05:00 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,13 +1,14 @@
-def introduction(name = 'Friend')
+def introduction(name)
  puts "Hi, my name is #{name}."
  end
 
-def introduction_with_language(name, language = 'computer')
+def introduction_with_language(name, language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}"
 end
 
 def introduction_with_language_optional(name, language = 'Ruby')
-puts 
+  puts "Hi, my name is #{name} and I am learning to program in #{language}"
+end
 
 
 
